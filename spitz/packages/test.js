7987f18266dc79e549d5544f736d36bb607ba5ab
CoderQC <73763394+CoderQCFromScraatch@users.noreply.github.com>
2021-10-30 13:18:51 -0700

    Update test.js

--- spitz/packages/test.js
+++ spitz/packages/test.js
@@ -1,22 +1,9 @@
-// START SPITZ LIBRARY
- var spitz = {}
- spitz.print = function(x) {
-   tempvar = "p" + x
- }
- 
- spitz.return = function() {
-   tempvar = "r1"
- }
-// END SPITZ LIBRARY
-
-
-
 // START WORKER HANDLER
 onmessage = function(arg) {
  args = arg.data
  
  
  // YOUR CODE BELOW
- spitz.print("Console")
- spitz.return()
+ tempvar = "pHello World"
+  tempvar = "r"
 };
